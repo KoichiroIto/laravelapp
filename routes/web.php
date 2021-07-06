de4e92c96cad32028ae7101cdc51d1a7b8f94873
KoichiroIto <k-ito@oic-ok.ac.jp>
2021-07-06 23:46:05 +0900

    add

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\Kadai2Controller;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('hello', [HelloController::class, 'index']);
-Route::post('hello',[HelloController::class, 'post']);
+Route::get('hello/add', [HelloController::class, 'add']);
+Route::post('hello/add', [HelloController::class, 'create']);
+Route::get('hello/edit/{id?}', [HelloController::class, 'edit']);
+Route::post('hello/edit/{id?}', [HelloController::class, 'update']);
+Route::get('hello/del/{id?}', [HelloController::class, 'del']);
+Route::post('hello/del/{id?}', [HelloController::class, 'remove']);
 
 Route::get('kadai2', [Kadai2Controller::class, 'index']);
 Route::post('kadai2',[Kadai2Controller::class, 'jyanken']);
